8987ba84996183f8d6e8c8c4ea376972d2713f1c
Mirasio <thibault.poncetta@gmail.com>
2020-11-23 14:09:23 +0100

    initial repo: maven project OK. Clear Project/Dep

---
+++ AgentChat/src/main/java/com/agent/chat/App.java
@@ -0,0 +1,11 @@
+package com.agent.chat;
+
+public class App 
+{
+    public static int test = 64;
+    
+    public static void main( String[] args )
+    {
+        System.out.println("Hello World!");
+    }
+}
