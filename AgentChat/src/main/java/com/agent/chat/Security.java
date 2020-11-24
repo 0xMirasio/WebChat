8fb60f28388217f190d0dbf8206583d9cab63c8f
Mirasio <thibault.poncetta@gmail.com>
2020-11-24 15:36:03 +0100

    finished login/creation of profil

---
+++ AgentChat/src/main/java/com/agent/chat/Security.java
@@ -0,0 +1,13 @@
+package com.agent.chat;
+
+public class Security {
+    public boolean verifyPassword(String password) {
+        FileOperation filework = new FileOperation();
+        String password_hash=  filework.getPassword();
+        String sig = filework.getSignature();
+        String password_hash_user_input = Integer.toString(password.hashCode());
+        String sig_user_input = Integer.toString(Integer.toString(password.hashCode()).hashCode());
+        
+        return (sig.equals(sig_user_input) && password_hash_user_input.equals(password_hash));
+    }
+}
