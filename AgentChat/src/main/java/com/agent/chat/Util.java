e6f581b24c5f166734285e591d351e9364c86250
Mirasio <thibault.poncetta@gmail.com>
2020-12-08 21:31:37 +0100

    help v.02

--- AgentChat/src/main/java/com/agent/chat/Util.java
+++ AgentChat/src/main/java/com/agent/chat/Util.java
@@ -17,25 +17,15 @@ public class Util {
 
     public List<String> transform2IPC(String listIPC, int nbUser) {
         List<String> IPC = new ArrayList<String>();
-        System.out.println("I WAS HERE BEFORE CRASHING1");
         String[] IPC_s = null;
-        System.out.println("I WAS HERE BEFORE CRASHING2");
         // listIPC = "['user-IP','user2-ip',...]"
-        IPC_s = listIPC.split("[", 2); // " 'user:ip', 'user2:ip']"
-        System.out.println("I WAS HERE BEFORE CRASHING3");
+        IPC_s = listIPC.split("\\[", 2); // " 'user:ip', 'user2:ip']"
         String IPC_temp = IPC_s[1];
-        System.out.println("I WAS HERE BEFORE CRASHING4");
         System.out.println(IPC_temp);
-        System.out.println("I WAS HERE BEFORE CRASHING5");
         IPC_temp = IPC_temp.split("]",2)[0]; // " 'user:ip', 'user2:ip'"
-        System.out.println("I WAS HERE BEFORE CRASHING6");
         System.out.println(IPC_temp);
-        System.out.println("I WAS HERE BEFORE CRASHING7");
         IPC_s = IPC_temp.split(",", nbUser); // [user:ip, user2:ip]
-        System.out.println("I WAS HERE BEFORE CRASHING8");
         for (String value : IPC_s) {
-            System.out.println("I WAS HERE BEFORE CRASHING9");
-            System.out.println(value);
             IPC.add(value);
         }
         System.out.println("[debug] IPC=" + IPC);
